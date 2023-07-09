f301bd70798d8d7cab95c46b46934a9b2bcad5c9
Vidhi Aggarwal <vidhiaggarwal397@gmail.com>
2023-07-09 22:01:02 +0530

    Aded project

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,9 +1,13 @@
 import React, { createContext, useEffect, useReducer, useState } from 'react'
+import { data } from '../data/Data'
 
 export const AppContext = createContext();
 const AppContextProvider = ({children}) => {
+  const [dataDsisplay,setDataDisplay] = useState(data);
+  
   return (
-    <AppContext.Provider>
+    <AppContext.Provider value={{dataDsisplay}}>
+    
         {children}
     </AppContext.Provider>
   )
